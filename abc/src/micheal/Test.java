36928554e85f52fcbf03b799af92dee283920dab
HoaNguyen <vanhoa8989@gmail.com>
2013-04-27 10:50:17 +0700

    bbbbb

---
+++ abc/src/micheal/Test.java
@@ -0,0 +1,24 @@
+package micheal;
+
+import java.util.Scanner;
+
+class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method
+		int a = 1;
+		int b[] = new int[2];
+		String c = new String();
+		System.out.print(c);
+		final double PI = 3.14;
+		Scanner scan = new Scanner (System.in);
+		String str   = scan.nextLine ();
+		System.out.print(str);
+
+
+	}
+
+}
